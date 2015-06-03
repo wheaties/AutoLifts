5de3d6eba0471f43f12c28e20ee6ec8851c27915
Owein Reese <oreese@mediamath.com>
2015-06-02 22:24:39 -0400

    setting up publishing

--- project/AutoLift.scala
+++ project/AutoLift.scala
@@ -22,4 +22,40 @@ object AutoLift{
   		  "-Ywarn-dead-code",
   		  "-Ywarn-value-discard")
 	  )
+
+  val publishSettings = Seq(
+    pomExtra := autoliftPom,
+    publishTo <<= version { v: String =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    pomIncludeRepository := { x => false },
+    publishMavenStyle := true,
+    publishArtifact in Test := false
+  )
+
+  val autoliftPom =
+    <url>http://github.com/wheaties/AutoLifts</url>
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:wheaties/AutoLifts.git</url>
+      <connection>scm:git:git@github.com:wheaties/AutoLifts.git</connection>
+    </scm>
+    <developers>
+      <developer>
+           <id>wheaties</id>
+           <name>Owein Reese</name>
+      </developer>
+    </developers>
+
 }
