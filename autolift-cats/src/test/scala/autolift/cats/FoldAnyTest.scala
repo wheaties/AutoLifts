c1244873ae136117cc4f782eb5b429ad5f509df4
frosforever <frosforever@users.noreply.github.com>
2016-01-09 17:06:12 -0500

    Fix import and spacing in Fold tests

--- autolift-cats/src/test/scala/autolift/cats/FoldAnyTest.scala
+++ autolift-cats/src/test/scala/autolift/cats/FoldAnyTest.scala
@@ -1,28 +1,27 @@
 package autolift.cats
 
-import cats._
 import cats.implicits._
-import autolift.cats._
+import autolift.Cats._
 
 class FoldAnyTest extends BaseSpec{
-	"foldAny on a List" should "work" in{
-		val in = List(1, 2, 3)
-		
-		assert(in.foldAny{x: Int => x < 2})
-		assert(!in.foldAny{x: Int => x < 0})
-	}
+  "foldAny on a List" should "work" in{
+    val in = List(1, 2, 3)
 
-	"foldAny on a List" should "work with functions" in{
-		val in = List(1, 2, 3)
-		
-		assert(in.foldAny{x: Any => true})
-		assert(!in.foldAny{x: Any => false})
-	}
+    assert(in.foldAny{x: Int => x < 2})
+    assert(!in.foldAny{x: Int => x < 0})
+  }
 
-	"foldAny on a List[Option]" should "work" in{
-		val in = List(Option(1), None, Option(2))
-		
-		assert(in.foldAny{x: Int => x < 2})
-		assert(!in.foldAny{x: Int => x < 0})
-	}
+  "foldAny on a List" should "work with functions" in{
+    val in = List(1, 2, 3)
+
+    assert(in.foldAny{x: Any => true})
+    assert(!in.foldAny{x: Any => false})
+  }
+
+  "foldAny on a List[Option]" should "work" in{
+    val in = List(Option(1), None, Option(2))
+
+    assert(in.foldAny{x: Int => x < 2})
+    assert(!in.foldAny{x: Int => x < 0})
+  }
 }
--- autolift-cats/src/test/scala/autolift/cats/FoldOverTest.scala
+++ autolift-cats/src/test/scala/autolift/cats/FoldOverTest.scala
@@ -1,21 +1,20 @@
 package autolift.cats
 
-import cats._
 import cats.implicits._
-import autolift.cats._
+import autolift.Cats._
 
 class FoldOverTest extends BaseSpec{
-	"foldOver on a List[Option] w/ List" should "work" in{
-		val in = List(Option(1), None)
-		val out = in.foldOver[List]
+  "foldOver on a List[Option] w/ List" should "work" in{
+    val in = List(Option(1), None)
+    val out = in.foldOver[List]
 
-		same[Option[Int]](out, Option(1))
-	}
+    same[Option[Int]](out, Option(1))
+  }
 
-	"foldOver on a List[Option] w/ Option" should "work" in{
-		val in = List(Option(1), None)
-		val out = in.foldOver[Option]
+  "foldOver on a List[Option] w/ Option" should "work" in{
+    val in = List(Option(1), None)
+    val out = in.foldOver[Option]
 
-		same[Int](out, 1)
-	}
+    same[Int](out, 1)
+  }
 }
