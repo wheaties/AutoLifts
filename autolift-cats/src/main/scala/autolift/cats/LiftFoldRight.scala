f33305c659cf331b137bcd9ba551123885261834
frosforever <frosforever@users.noreply.github.com>
2016-01-09 18:31:46 -0500

    Modify FoldRight for cats Eval lazy

--- autolift-cats/src/main/scala/autolift/cats/LiftFoldRight.scala
+++ autolift-cats/src/main/scala/autolift/cats/LiftFoldRight.scala
@@ -1,6 +1,6 @@
 package autolift.cats
 
-import cats.{Functor, Foldable}
+import cats.{Eval, Functor, Foldable}
 import autolift.LiftFoldRight
 import export._
 
@@ -12,12 +12,11 @@ object CatsLiftFoldRight extends LowPriorityCatsLiftFoldRight{
   def apply[FA, Fn, Z](implicit lift: CatsLiftFoldRight[FA, Fn, Z]): Aux[FA, Fn, Z, lift.Out] = lift
 
   @export(Subclass)
-  implicit def base[F[_], A, C >: A, B](implicit fold: Foldable[F]): Aux[F[A], (C, => B) => B, B, B] =
-    new CatsLiftFoldRight[F[A], (C, => B) => B, B]{
-      type Out = B
+  implicit def base[F[_], A, C >: A, B](implicit fold: Foldable[F]): Aux[F[A], (C, Eval[B]) => Eval[B], Eval[B], Eval[B]] =
+    new CatsLiftFoldRight[F[A], (C, Eval[B]) => Eval[B], Eval[B]]{
+      type Out = Eval[B]
 
-      //TODO: Update to Eval lazyFoldRight cats-like
-      def apply(fa: F[A], f: (C, => B) => B, z: B) = ??? //fold.foldRight(fa, z)(f)
+      def apply(fa: F[A], f: (C, Eval[B]) => Eval[B], z: Eval[B]) = fold.foldRight(fa, z)(f)
     }
 }
 
@@ -35,15 +34,15 @@ trait LowPriorityCatsLiftFoldRight{
 
 trait LiftFoldRightSyntax{
   implicit class LiftFoldRightOps[F[_], A](fa: F[A]){
-    def liftFoldRight[B, Z](z: Z)(f: (B, => Z) => Z)(implicit lift: LiftFoldRight[F[A], (B, => Z) => Z, Z]): lift.Out =
+    def liftFoldRight[B, Z](z: Eval[Z])(f: (B, Eval[Z]) => Eval[Z])(implicit lift: LiftFoldRight[F[A], (B, Eval[Z]) => Eval[Z], Eval[Z]]): lift.Out =
       lift(fa, f, z)
   }
 }
 
-final class LiftedFoldRight[B, Z](z: Z, f: (B, => Z) => Z){
-  def apply[That](that: That)(implicit lift: LiftFoldRight[That, (B, => Z) => Z, Z]): lift.Out = lift(that, f, z)
+final class LiftedFoldRight[B, Z](z: Eval[Z], f: (B, Eval[Z]) => Eval[Z]) {
+  def apply[That](that: That)(implicit lift: LiftFoldRight[That, (B, Eval[Z]) => Eval[Z], Eval[Z]]): lift.Out = lift(that, f, z)
 }
 
-trait LiftFoldRightContext{
-  def liftFoldRight[B, Z](z: Z)(f: (B, => Z) => Z) = new LiftedFoldRight(z, f)
+trait LiftFoldRightContext {
+  def liftFoldRight[B, Z](z: Eval[Z])(f: (B, Eval[Z]) => Eval[Z]) = new LiftedFoldRight(z, f)
 }
--- autolift-cats/src/test/scala/autolift/cats/LiftFoldRightTest.scala
+++ autolift-cats/src/test/scala/autolift/cats/LiftFoldRightTest.scala
@@ -1,21 +1,22 @@
 package autolift.cats
 
+import cats.{Eval, Now}
 import cats.implicits._
 import autolift.Cats._
 
 class LiftFoldRightTest extends BaseSpec{
-	def plus(x: Int, y: => Int) = x + y
+	def plus(x: Int, y: Eval[Int]) = y.map(_ + x)
 
 	"liftFoldRight on a List" should "work" in{
 		val in = List(1, 2, 3)
-		val out = in.liftFoldRight(0)(plus)
+		val out = in.liftFoldRight(Now(0))(plus).value
 
 		same[Int](out, 6)
 	}
 
 	"liftFoldRight on a List[Option]" should "work" in{
 		val in = List(Option(1), Option(2), None)
-		val out = in.liftFoldRight(3)(plus)
+		val out = in.liftFoldRight(Now(3))(plus).map(_.value)
 
 		same[List[Int]](out, List(4, 5, 3))
 	}
