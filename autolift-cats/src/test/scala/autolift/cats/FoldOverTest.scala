2cd829c03d53f07feb6a0a6c464e0107a590afd6
frosforever <frosforever@users.noreply.github.com>
2016-01-09 17:02:34 -0500

    Revert "Remove Fold tests for now to focus on Lift. REVERT later"
    This reverts commit 301ff444ad181994b8a680f42ad2d55da471ab9c.

---
+++ autolift-cats/src/test/scala/autolift/cats/FoldOverTest.scala
@@ -0,0 +1,21 @@
+package autolift.cats
+
+import cats._
+import cats.implicits._
+import autolift.cats._
+
+class FoldOverTest extends BaseSpec{
+	"foldOver on a List[Option] w/ List" should "work" in{
+		val in = List(Option(1), None)
+		val out = in.foldOver[List]
+
+		same[Option[Int]](out, Option(1))
+	}
+
+	"foldOver on a List[Option] w/ Option" should "work" in{
+		val in = List(Option(1), None)
+		val out = in.foldOver[Option]
+
+		same[Int](out, 1)
+	}
+}
