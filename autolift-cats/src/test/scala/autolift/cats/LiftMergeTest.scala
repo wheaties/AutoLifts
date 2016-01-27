b72aeccbd5777b3ac7ccefdb98766b36b0591261
Owein Reese <owreese@gmail.com>
2016-01-26 21:38:46 -0500

    fix unit tests

--- autolift-cats/src/test/scala/autolift/cats/LiftMergeTest.scala
+++ autolift-cats/src/test/scala/autolift/cats/LiftMergeTest.scala
@@ -3,17 +3,17 @@ package autolift.cats
 import autolift.Cats._
 import cats.implicits._
 
-class LiftJoinTest extends BaseSpec{
-	"liftJoin on a Option[Int] from a Option[Int]" should "work" in{
+class LiftMergeTest extends BaseSpec{
+	"liftMerge on a Option[Int] from a Option[Int]" should "work" in{
 		val in = Option(1)
-		val out = in liftJoin in
+		val out = in liftMerge in
 
 		same[Option[(Int,Int)]](out, Option((1, 1)))
 	}
 
-	"liftJoin on a Option[List[Int]] on a List[Int]" should "work" in{
+	"liftMerge on a Option[List[Int]] on a List[Int]" should "work" in{
 		val in = Option(List(1, 2))
-		val out = in liftJoin List(1, 2)
+		val out = in liftMerge List(1, 2)
 
 		same[Option[List[(Int,Int)]]](out, Option(List((1, 1), (2, 1), (1, 2), (2, 2))))
 	}
--- autolift-scalaz/src/test/scala/autolift/scalaz/LiftMergeWithTest.scala
+++ autolift-scalaz/src/test/scala/autolift/scalaz/LiftMergeWithTest.scala
@@ -4,31 +4,31 @@ import scalaz._
 import Scalaz._
 import autolift.Scalaz._
 
-class LiftJoinWithTest extends BaseSpec{
+class LiftMergeWithTest extends BaseSpec{
 
-	"liftJoinWith on a Option[Int] from a Option[Int]" should "work" in{
+	"liftMergeWith on a Option[Int] from a Option[Int]" should "work" in{
 		val in = Option(1)
-		val out = in.liftJoinWith(Option(1))(intintF)
+		val out = in.liftMergeWith(Option(1))(intintF)
 
 		same[Option[Int]](out, Option(2))
 	}
 
-	"liftJoinWith on a Option[List] on a List" should "work" in{
+	"liftMergeWith on a Option[List] on a List" should "work" in{
 		val in = Option(List(1))
-		val out = in.liftJoinWith(List(1))(intintF)
+		val out = in.liftMergeWith(List(1))(intintF)
 
 		same[Option[List[Int]]](out, Option(List(2)))
 	}
 
-	"LiftedJoinWith on a Option[List]" should "work" in{
-		val lf = liftJoinWith(intintF)
+	"LiftedMergeWith on a Option[List]" should "work" in{
+		val lf = liftMergeWith(intintF)
 		val out = lf(Option(List(1)), List(1))
 
 		same[Option[List[Int]]](out, Option(List(2)))
 	}
 
-	"LiftedJoinWith" should "map" in{
-		val lf = liftJoinWith(intintF)
+	"LiftedMergeWith" should "map" in{
+		val lf = liftMergeWith(intintF)
 		val lf2 = lf map (_ + 1)
 		val out = lf2(Option(List(1)), List(1))
 
