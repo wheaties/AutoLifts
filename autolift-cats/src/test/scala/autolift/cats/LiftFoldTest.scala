d425f9f8bd0680d9112ca4d2fad1f8d43de50499
Wheaties <owreese@gmail.com>
2016-05-08 22:05:10 -0400

    combining LiftFold and LiftFoldAt

--- autolift-cats/src/test/scala/autolift/cats/LiftFoldTest.scala
+++ autolift-cats/src/test/scala/autolift/cats/LiftFoldTest.scala
@@ -1,9 +1,17 @@
 package autolift.cats
 
 import cats.implicits._
+import cats.data.Xor
 import autolift.Cats._
 
 class LiftFoldTest extends BaseSpec{
+  "liftFold[List] on a List" should "work" in{
+    val in = List(1, 2, 3)
+    val out = in.liftFold[List]
+
+    same[Int](out, 6)
+  }
+
   "liftFold on a List" should "work" in{
     val in = List(1, 2, 3)
     val out = in.liftFold
@@ -16,5 +24,26 @@ class LiftFoldTest extends BaseSpec{
     val out = in.liftFold
 
     same[List[Int]](out, List(1))
+  }
+
+  "liftFold[List] on a List[Option] w/ List" should "work" in{
+    val in = List(Option(1), None, Option(2))
+    val out = in.liftFold[List]
+
+    same[Option[Int]](out, Option(3))
+  }
+
+  "liftFold[Option] on a List[Option] w/ Option" should "work" in{
+    val in = List(Option(1), None, Option(2))
+    val out = in.liftFold[Option]
+
+    same[List[Int]](out, List(1, 0, 2))
+  }
+
+  "liftFold[List] on a Xor[List]" should "work" in{
+    val in = Xor.right(List(1, 2))
+    val out = in.liftFold[List]
+
+    same[Xor[Nothing,Int]](out, Xor.right(3))
   }
 }
--- autolift-scalaz/src/test/scala/autolift/scalaz/LiftFoldTest.scala
+++ autolift-scalaz/src/test/scala/autolift/scalaz/LiftFoldTest.scala
@@ -19,10 +19,45 @@ class LiftFoldTest extends BaseSpec{
 		same[List[Int]](out, List(1))
 	}
 
+	"liftFold[List] on an Option[Option[List[List]]]" should "work" in{
+		val in = Option(Option(List(Nil, Nil, List(1))))
+		val out = in.liftFold[List]
+
+		same[Option[Option[List[Int]]]](out, Option(Option(List(1))))
+	}
+
 	"liftFold on a State[List]" should "work" in{
 		val in: State[Int,List[Int]] = State.state[Int,List[Int]](List(1, 2, 3))
 		val out = in.liftFold
 
 		same[Int](out.run(1)._2, 6)
+	}
+
+	"liftFold[List] on a List" should "work" in{
+		val in = List(1, 2, 3)
+		val out = in.liftFold[List]
+
+		same[Int](out, 6)
+	}
+
+	"liftFold[List] on a List[Option] w/ List" should "work" in{
+		val in = List(Option(1), None, Option(2))
+		val out = in.liftFold[List]
+
+		same[Option[Int]](out, Option(3))
+	}
+
+	"liftFold[Option] on a List[Option] w/ Option" should "work" in{
+		val in = List(Option(1), None, Option(2))
+		val out = in.liftFold[Option]
+
+		same[List[Int]](out, List(1, 0, 2))
+	}
+
+	"liftFold[List] on a Disjunction[List]" should "work" in{
+		val in: Int \/ List[Int] = \/.right(List(1, 2, 3))
+		val out = in.liftFold[List]
+
+		same[Int \/ Int](out, \/.right(6))
 	}
 }
