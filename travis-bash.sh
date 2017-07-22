a731fdb1a45d51574937917b8cbfcae13bcfab62
Wheaties <owreese@gmail.com>
2017-07-22 13:39:12 -0400

    that didn't work...

--- travis-bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$TRAVIS_JDK_VERSION" = "*7*" ]]; then
-  exec sbt clean compile test
-fi
-
-if [[ "$TRAVIS_JDK_VERSION" = "*8*" ]]; then
-  exec sbt clean compile tut coverage test && sbt coverageAggregate
-fi
